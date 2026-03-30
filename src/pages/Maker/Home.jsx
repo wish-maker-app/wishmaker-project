9983434957ac6586b3a891aae963350d4b42c5d3
mathis33700 <mathis33700@gmail.com>
2026-03-30 08:19:36 +0200

    trigger redeploy

--- src/pages/Maker/Home.jsx
+++ src/pages/Maker/Home.jsx
@@ -1,4 +1,4 @@
-// Wish Maker v1.3
+// Wish Maker v1.4
 import { useState, useEffect, useRef, useCallback } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
